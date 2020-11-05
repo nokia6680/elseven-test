3981b075696ad0595866ddd1da03a160007e8a71
b0rntokill <zappbranigan39@gmail.com>
2020-11-05 14:17:46 +0200

    Стили всех ховеров, первая верси js

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -38,8 +38,39 @@ for (var i = 0; i < upperItem.length; i++) {
 var priceOpener = document.querySelector('.filter__price-open');
 var priceOverlay = document.querySelector('.filter__price-body');
 
-priceOpener.addEventListener('click', function () {
-  priceOverlay.classList.toggle('active');
-  priceOpener.classList.toggle('active');
-  return false;
+if (priceOpener) {
+    priceOpener.addEventListener('click', function () {
+        priceOverlay.classList.toggle('active');
+        priceOpener.classList.toggle('active');
+        return false;
+    });
+};
+
+$(function() {
+    var quantityBlock = $('.cards__input-quantity');
+    var quantityInput = quantityBlock.find('input');
+
+    quantityBlock.on('click', function (evt) {
+        evt.preventDefault();
+        if (evt.target.classList.contains('plus')) {
+            quantityInput.val(parseFloat(quantityInput.val()) + 1);
+        }
+        if (evt.target.classList.contains('minus') && parseFloat(quantityInput.val()) !== 1) {
+            quantityInput.val(parseFloat(quantityInput.val()) - 1);
+        }
+    });
+});
+
+$(function () {
+    $('.table div:not(.body-head, .body-title, .aside-empty-title, .body-main-title, .table-title, .table-main-title, .table-body, .body-photo, .body-photo-block)').on('mouseenter mouseleave', function (evt) {
+        if (evt.target.classList.contains('body-content') && !$(this).hasClass('body-block')) {
+            var bodyIndex = $(this).index() + 1;
+            $(this).parents('.body-block').toggleClass('hover');
+            $('.body-block').children(".body-content:nth-of-type(" + bodyIndex  + ")").toggleClass("hover-col");
+        }
+        if (evt.currentTarget.classList.contains('body-block')) {
+            var titleIndex = $(this).index();
+            $('.table-aside').children(".title-block:nth-of-type(" + titleIndex  + ")").toggleClass("hover");
+        }
+    });
 });
